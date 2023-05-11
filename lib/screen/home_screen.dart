59110c5f29f04edc1f65408721246f09ee89f43f
sig03 <sig03@naver.com>
2023-05-11 14:50:32 +0900

    first commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/component/main_calendar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
+  }
+}
